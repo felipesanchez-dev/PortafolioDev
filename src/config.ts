a547a712eba8fb6da9e4f977f69cb953c5ce9e42
felipesanchez-dev <jfelipe9.121@gmail.com>
2024-06-07 12:27:30 -0500

    Main push and deploy vercel

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Portafolio web | Juan Sanchez';
+export const SITE_DESCRIPTION = 'Portafolio web Juan felipe reyes sanchez, programador FullSatck Colombia';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
